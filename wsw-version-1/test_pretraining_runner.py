48636fc0d9f2165c86e0d46f389912979e8078cc
Prasan Yapa <toprasanyapa@gmail.com>
2025-07-28 17:13:35 +0900

    Update test_pretraining_runner.py
    Adding unit testing logic for ESR.

--- wsw-version-1/test_pretraining_runner.py
+++ wsw-version-1/test_pretraining_runner.py
@@ -5,7 +5,13 @@ import pytest
 import tensorflow as tf
 
 import modeling_speaker as modeling
-from pretraining_runner import get_speaker_utterance_identification_output
+from pretraining_runner import get_speaker_utterance_identification_output, get_exact_speaker_recognition_output
+
+
+class DummyBertConfig:
+    hidden_size = 8
+    hidden_act = "gelu"
+    initializer_range = 0.02
 
 
 def dummy_gather_indexes(sequence_tensor, positions):
@@ -18,6 +24,23 @@ def dummy_gather_indexes(sequence_tensor, positions):
 
 def dummy_layer_norm(x):
     return x
+
+
+@pytest.fixture
+def dummy_inputs():
+    batch_size = 2
+    seq_length = 6
+    max_utr_num = 3
+    hidden_size = 8
+    max_predictions = 2
+
+    input_tensor = tf.constant(np.random.rand(batch_size, seq_length, hidden_size), dtype=tf.float32)
+    cls_positions = tf.constant([[0, 2, 4], [1, 3, 5]], dtype=tf.int32)  # shape [2, 3]
+    masked_sr_positions = tf.constant([[0], [1]], dtype=tf.int32)  # shape [2, 1]
+    masked_sr_labels = tf.constant([[1], [2]], dtype=tf.int32)
+    masked_sr_weights = tf.constant([[1.0], [1.0]], dtype=tf.float32)
+
+    return DummyBertConfig(), input_tensor, cls_positions, masked_sr_positions, masked_sr_labels, masked_sr_weights
 
 
 @pytest.mark.parametrize("batch_size,max_utr_num,width,max_predictions_per_seq_ar", [
@@ -60,3 +83,24 @@ def test_get_speaker_utterance_identification_output(batch_size, max_utr_num, wi
     assert isinstance(loss_val, float)
     assert per_example_loss_val.shape[0] == batch_size * max_predictions_per_seq_ar
     assert log_probs_val.shape[1] == max_utr_num
+
+
+def test_get_exact_speaker_recognition_output(dummy_inputs):
+    bert_config, input_tensor, cls_positions, masked_sr_positions, masked_sr_labels, masked_sr_weights = dummy_inputs
+
+    tf.compat.v1.reset_default_graph()
+    with tf.compat.v1.Session() as sess:
+        loss, per_example_loss, log_probs = get_exact_speaker_recognition_output(
+            bert_config,
+            input_tensor,
+            cls_positions,
+            masked_sr_positions,
+            masked_sr_labels,
+            masked_sr_weights
+        )
+        sess.run(tf.compat.v1.global_variables_initializer())
+        loss_val, pel_val, logp_val = sess.run([loss, per_example_loss, log_probs])
+
+        assert loss_val > 0
+        assert pel_val.shape[0] == 2
+        assert logp_val.shape == (2, 3)
