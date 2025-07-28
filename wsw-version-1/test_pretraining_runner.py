81f43bc502d4270765616143b0ad75f3cda7a9db
Prasan Yapa <toprasanyapa@gmail.com>
2025-07-28 18:16:33 +0900

    Update test_pretraining_runner.py
    Adding unit testing logic for RUND.

--- wsw-version-1/test_pretraining_runner.py
+++ wsw-version-1/test_pretraining_runner.py
@@ -5,7 +5,8 @@ import pytest
 import tensorflow as tf
 
 import modeling_speaker as modeling
-from pretraining_runner import get_speaker_utterance_identification_output, get_exact_speaker_recognition_output
+from pretraining_runner import (get_speaker_utterance_identification_output, get_exact_speaker_recognition_output,
+                                get_root_utterance_node_detection_output)
 
 
 class DummyBertConfig:
@@ -39,8 +40,10 @@ def dummy_inputs():
     masked_sr_positions = tf.constant([[0], [1]], dtype=tf.int32)  # shape [2, 1]
     masked_sr_labels = tf.constant([[1], [2]], dtype=tf.int32)
     masked_sr_weights = tf.constant([[1.0], [1.0]], dtype=tf.float32)
+    root_labels = tf.constant(np.random.randint(0, 2, size=(batch_size,)), dtype=tf.int32)
 
-    return DummyBertConfig(), input_tensor, cls_positions, masked_sr_positions, masked_sr_labels, masked_sr_weights
+    return (DummyBertConfig(), input_tensor, cls_positions, masked_sr_positions, masked_sr_labels, masked_sr_weights,
+            root_labels)
 
 
 @pytest.mark.parametrize("batch_size,max_utr_num,width,max_predictions_per_seq_ar", [
@@ -68,7 +71,11 @@ def test_get_speaker_utterance_identification_output(batch_size, max_utr_num, wi
     adr_recog_labels = tf.constant(np.zeros((batch_size, max_predictions_per_seq_ar), dtype=np.int32))
     adr_recog_weights = tf.constant(np.ones((batch_size, max_predictions_per_seq_ar), dtype=np.float32))
 
-    with tf.compat.v1.Session() as sess:
+    tf.config.experimental.set_virtual_device_configuration(tf.config.experimental.list_physical_devices('GPU')[0], [
+        tf.config.experimental.VirtualDeviceConfiguration(memory_limit=20480)])
+    config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    with tf.compat.v1.Session(config=config) as sess:
         loss, per_example_loss, log_probs = get_speaker_utterance_identification_output(
             bert_config,
             input_tensor,
@@ -89,7 +96,11 @@ def test_get_exact_speaker_recognition_output(dummy_inputs):
     bert_config, input_tensor, cls_positions, masked_sr_positions, masked_sr_labels, masked_sr_weights = dummy_inputs
 
     tf.compat.v1.reset_default_graph()
-    with tf.compat.v1.Session() as sess:
+    tf.config.experimental.set_virtual_device_configuration(tf.config.experimental.list_physical_devices('GPU')[0], [
+        tf.config.experimental.VirtualDeviceConfiguration(memory_limit=20480)])
+    config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    with tf.compat.v1.Session(config=config) as sess:
         loss, per_example_loss, log_probs = get_exact_speaker_recognition_output(
             bert_config,
             input_tensor,
@@ -104,3 +115,23 @@ def test_get_exact_speaker_recognition_output(dummy_inputs):
         assert loss_val > 0
         assert pel_val.shape[0] == 2
         assert logp_val.shape == (2, 3)
+
+
+def test_get_root_utterance_node_detection_output(dummy_inputs):
+    bert_config, input_tensor, root_labels = dummy_inputs
+
+    tf.compat.v1.reset_default_graph()
+    with tf.compat.v1.Session() as sess:
+
+        loss, per_example_loss, log_probs = get_root_utterance_node_detection_output(
+            bert_config, input_tensor, root_labels
+        )
+
+        sess.run(tf.compat.v1.global_variables_initializer())
+        loss_val, pel_val, logp_val = sess.run([loss, per_example_loss, log_probs])
+
+        # Assertions
+        assert isinstance(loss_val, np.floating)
+        assert pel_val.shape == (3,)
+        assert logp_val.shape == (3, 2)
+        assert np.allclose(np.exp(logp_val).sum(axis=1), 1.0, atol=1e-5)
